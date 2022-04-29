6aa5d3a1356d0e6d0e1fb43b6ada94e81f18b325
d.turovatov <d.turovatov@rt-solar.ru>
2022-04-29 16:20:43 +0300

    solve 02-components 01-07 tasks

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,45 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  methods: {
+    formatAsLocalDate(timestamp) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      const lang = navigator.language;
+      return new Date(timestamp).toLocaleDateString(lang, options);
+    },
+    formatAsIsoDate(timestamp) {
+      return new Date(timestamp).toISOString().split('T')[0];
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="formatAsIsoDate(date)">{{ formatAsLocalDate(date) }}</time>
       </li>
     </ul>`,
 });
--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupView from '../06-MeetupView/MeetupView';
+import { fetchMeetupById } from './meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,55 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  data() {
+    return {
+      method: null,
+      isLoading: false,
+      error: false,
+    };
+  },
+
+  watch: {
+    meetupId: {
+      immediate: true,
+      async handler(value) {
+        this.meetup = null;
+        this.error = null;
+        this.isLoading = true;
+
+        try {
+          this.meetup = await fetchMeetupById(value);
+        } catch (e) {
+          this.error = e.message;
+        } finally {
+          this.isLoading = false;
+        }
+      },
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <meetup-view :meetup="meetup" v-if="meetup"/>
 
-      <ui-container>
-        <ui-alert>Загрузка...</ui-alert>
-      </ui-container>
+      <template v-else>
+        <ui-container v-if="isLoading">
+          <ui-alert>Загрузка...</ui-alert>
+        </ui-container>
 
-      <ui-container>
-        <ui-alert>error</ui-alert>
-      </ui-container>
+        <ui-container v-else-if="error">
+          <ui-alert>{{ error }}</ui-alert>
+        </ui-container>
+      </template>
     </div>`,
 });
